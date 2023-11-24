118e9334c47a416201463bcae45bd3d9971347d0
Christian Berendt <berendt@osism.tech>
2023-11-24 20:12:56 +0100

    Use only the filename
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- src/render-images.py
+++ src/render-images.py
@@ -9,7 +9,7 @@ import yaml
 MANAGER_VERSION = os.environ.get("MANAGER_VERSION", None)
 if not MANAGER_VERSION:
     try:
-        with open("environments/manager/configuration.yml") as fp:
+        with open("configuration.yml") as fp:
             data = yaml.load(fp, Loader=yaml.FullLoader)
 
         MANAGER_VERSION = data["manager_version"]
