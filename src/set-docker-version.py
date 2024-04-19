33dfc099e1f29b89739ee86214db16b90775b65b
Christian Berendt <berendt@osism.tech>
2024-04-19 22:31:35 +0200

    Add script to set the docker version (#418)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ src/set-docker-version.py
@@ -0,0 +1,41 @@
+import os
+import re
+
+import requests
+import yaml
+
+# get environment parameters
+MANAGER_VERSION = os.environ.get("MANAGER_VERSION", None)
+if not MANAGER_VERSION:
+    try:
+        with open("configuration.yml") as fp:
+            data = yaml.load(fp, Loader=yaml.FullLoader)
+
+        MANAGER_VERSION = data["manager_version"]
+    except:
+        MANAGER_VERSION = "latest"
+
+VERSIONS_URL = os.environ.get(
+    "VERSIONS_URL",
+    "https://raw.githubusercontent.com/osism/release/main/%s/base.yml"
+    % MANAGER_VERSION,  # noqa E501
+)
+
+# load versions files from release repository
+r = requests.get(VERSIONS_URL)
+versions = yaml.full_load(r.text)
+
+docker_version = versions["osism_projects"]["docker"]
+
+print(
+    "The docker_version parameter in environments/configuration.yml is modified"
+    " if available there. If the docker_version is configured elsewhere, it must"
+    " be adjusted manually.\n\n"
+    f"The Docker version for OSISM {MANAGER_VERSION} is '{docker_version}'."
+)
+
+with open("configuration.yml", "r+") as fp:
+    data = fp.read()
+    data = re.sub("^docker_version: .*$", f"docker_version: '{docker_version}'", data)
+    fp.seek(0)
+    fp.write(data)
