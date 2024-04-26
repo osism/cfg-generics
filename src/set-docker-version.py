0efc74f3feaa9cbdb202426ebe37ec5edb2fcdcd
Christian Berendt <berendt@osism.tech>
2024-04-26 16:46:10 +0200

    Enable docker_version management (#424)
    This sets the docker_version as it is defined in osism/release.
    According to the manager version.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- src/set-docker-version.py
+++ src/set-docker-version.py
@@ -28,14 +28,31 @@ versions = yaml.full_load(r.text)
 docker_version = versions["osism_projects"]["docker"]
 
 print(
-    "The docker_version parameter in environments/configuration.yml is modified"
+    "The docker_version parameter in environments/configuration.yml and"
+    " environments/manager/configuration.yml is modified"
     " if available there. If the docker_version is configured elsewhere, it must"
     " be adjusted manually.\n\n"
     f"The Docker version for OSISM {MANAGER_VERSION} is '{docker_version}'."
 )
 
-with open("configuration.yml", "r+") as fp:
-    data = fp.read()
-    data = re.sub("^docker_version: .*$", f"docker_version: '{docker_version}'", data)
-    fp.seek(0)
-    fp.write(data)
+try:
+    with open("configuration.yml", "r+") as fp:
+        data = fp.read()
+        data = re.sub(
+            "^docker_version: .*$", f"docker_version: '{docker_version}'", data
+        )
+        fp.seek(0)
+        fp.write(data)
+except FileNotFoundError:
+    pass
+
+try:
+    with open("manager/configuration.yml", "r+") as fp:
+        data = fp.read()
+        data = re.sub(
+            "^docker_version: .*$", f"docker_version: '{docker_version}'", data
+        )
+        fp.seek(0)
+        fp.write(data)
+except FileNotFoundError:
+    pass
